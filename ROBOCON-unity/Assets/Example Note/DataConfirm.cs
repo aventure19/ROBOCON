a018ac223e391e5230a42ca9da3bb7ef509fb985
aventure19 <hideride@outlook.jp>
2017-09-03 17:32:52 +0900

    Unityプロジェクトの追加

---
+++ ROBOCON-unity/Assets/Example Note/DataConfirm.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DataConfirm : MonoBehaviour {
+
+    public string data;
+    
+    public string source;
+    public string status;
+    public string data1;
+    public string data2;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ ROBOCON-unity/Assets/Example Note/NoteIndicator.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using MidiJack;
+
+public class NoteIndicator : MonoBehaviour
+{
+    public int noteNumber;
+    public float velocity_f = 0.0f;
+
+    void Update()
+    {
+        transform.localScale = Vector3.one * (0.1f + MidiMaster.GetKey(noteNumber));
+
+        velocity_f = MidiMaster.GetKey(noteNumber);
+
+        var color = MidiMaster.GetKeyDown(noteNumber) ? Color.red : Color.white;
+        GetComponent<Renderer>().material.color = color;
+    }
+}
